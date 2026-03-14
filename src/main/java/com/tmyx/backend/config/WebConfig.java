b1a05db0521f340e21fcf0462035455864c2d99a
TMyxGames <tmyxgames@gmail.com>
2026-03-14 20:15:41 +0800

    修复加载文章需要token的问题

--- src/main/java/com/tmyx/backend/config/WebConfig.java
+++ src/main/java/com/tmyx/backend/config/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "/auth/sendCaptcha",        // 发送验证码
                         "/auth/login",              // 登录
                         "/article/published",       // 获取已发布文章
+                        "/article/get/**",          // 获取文章详情
                         "/files/**"                 // 文件路径映射
 
 
